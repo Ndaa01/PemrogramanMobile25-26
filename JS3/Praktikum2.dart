9e7b8de2bbeed597432c83c2e11de87f0d4ab1f7
Ndaa01 <ananda.idn12345@gmail.com>
2025-09-10 12:12:15 +0700

    Menampilkan Set dan map yang Kosong

--- JS3/Praktikum2.dart
+++ JS3/Praktikum2.dart
@@ -1,5 +1,10 @@
 void main(List<String> args) {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-print(halogens);
+  var names1 = <String>{};
+Set<String> names2 = {}; // This works, too.
+var names3 = {}; // Creates a map, not a set.
+
+print(names1);
+print(names2);
+print(names3);
 }
-// Akan terdapat output berupa isi dari halogens
+// Terdapat set dan map yang kosong
