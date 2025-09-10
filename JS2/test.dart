6385e97c6caf9354306ef13ba03fd2b802cb1534
Ndaa01 <ananda.idn12345@gmail.com>
2025-09-10 11:35:03 +0700

    Tidak ada angka yang tercetak ke console, karena setiap iterasi terkena continue, dan saat sampai 21, langsung break.

---
+++ JS2/test.dart
@@ -0,0 +1,18 @@
+void main() {
+  String set = "test2";
+  if (set == "test1") {
+    print("Test1");
+  } else if (set == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+  
+  if (set == "test2") print("Test2 again");
+
+String test = "true";
+if (test == "true") {
+   print("Kebenaran");
+}
+
+}
